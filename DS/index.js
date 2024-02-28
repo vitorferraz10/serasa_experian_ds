8fc8e6a2694800b62d84e04ca8035f03ee910c55
Vitor Ferraz <vitor.maciel@fretebras.com.br>
2024-02-28 16:02:14 -0300

    feat: first commit with add tokens ds

---
+++ DS/index.js
@@ -0,0 +1,7 @@
+const colors = require('./src/tokens/colors');
+const typography = require('./src/tokens/typography');
+
+module.exports = {
+  colors,
+  typography,
+};
---
+++ DS/src/tokens/colors.js
@@ -0,0 +1,16 @@
+module.exports = {
+  text: {
+    brand: {
+      default: "#E63888",
+    },
+    neutral: {
+      light_solid: "#FFFFFF",
+      light_high: "rgba(255,255,255,0.80)",
+    },
+    dark: {
+      dark_low: "rgba(0,0,0,0.44)",
+      dark_medium: "rgba(0,0,0,0.60)",
+      dark_high: "rgba(0,0,0,0.80)",
+    },
+  },
+};
